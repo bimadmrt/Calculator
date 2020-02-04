f2b1d6cd94f015cf68ae38fa6406de031cad8278
bimadmrt <56182626+bimadmrt@users.noreply.github.com>
2020-02-04 08:20:56 +0700

    File src
    bima demarta aisy - 183140914111022

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h1>／人◕ ‿‿ ◕人＼</h1>
+    <h1>بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ</h1>
+    <Calculator />
+    <h1>الْحَمْدُ لِلَّهِ رَبِّ الْعَالَمِينَ</h1>
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Calculator
+  },
+  data () {
+    return ({
+      message: null
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
